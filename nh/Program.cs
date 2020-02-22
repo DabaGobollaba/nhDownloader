d3c403a3ae8a9e605bcfe828c8be84bba2ac30a8
Markus Koller <if17b019@technikum-wien.at>
2020-02-22 14:48:34 +0100

    added random features

--- nh/Program.cs
+++ nh/Program.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using NHentaiSharp.Core;
 using NHentaiSharp.Search;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 using iText.IO.Image;
 using iText.Kernel.Geom;
@@ -11,11 +12,14 @@ using iText.Layout;
 using iText.Layout.Element;
 using System.IO;
 using System.Collections.Generic;
+using NHentaiSharp.Exception;
 
 namespace nh
 {
     class Program
     {
+        private static readonly Random _rnd = new Random();
+
         static async Task Main(string[] args)
         {
             while (true)
@@ -23,47 +27,157 @@ namespace nh
                 Console.Write("Enter an id: ");
 
                 string strInput = Console.ReadLine();
-                int id;
 
-                if (!int.TryParse(strInput, out id))
+                Regex commaSeparatedNumbers = new Regex("^([0-9]{1,6},[0-9]{1,6}(,[0-9]{1,6})*)+$");
+
+                // if list of ids
+                if (commaSeparatedNumbers.IsMatch(strInput))
                 {
-                    Console.WriteLine("Id was not parsable to an int! Try again... \n");
-                    continue;
+                    var ids = strInput.Split(",");
+                    var doujins = new List<GalleryElement>();
+                    foreach (var id in ids)
+                    {
+                        try
+                        {
+                            doujins.Add(await SearchClient.SearchByIdAsync(int.Parse(id)));
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine(id + " not found.");
+                        }
+                    }
+                    await Multiple(doujins);
                 }
-
-                // Search for doujin and print out info
-                GalleryElement doujin;
-                try
+                else if (strInput == "random")
                 {
-                    doujin = await SearchClient.SearchByIdAsync(id);
+                    try
+                    {
+                        await GetRandom();
+                    }
+                    catch (InvalidArgumentException)
+                    {
+                        continue;
+                    }
                 }
-                catch (Exception)
+                else if (strInput == "taglist")
                 {
-                    Console.WriteLine("Error finding doujin...\n");
-                    continue;
+                    try
+                    {
+                        await GetRandomWithTags();
+                    }
+                    catch (InvalidArgumentException)
+                    {
+                        continue;
+                    }
                 }
-
-                PrintDoujinInfo(doujin);
-                Console.WriteLine();
-
-                // Get filename
-                Console.Write("Enter a filename (optional): ");
-                string filename = Console.ReadLine();
-
-                if (filename == string.Empty)
+                // if no list of ids
+                else
                 {
-                    filename = id.ToString();
-                }
-                if (System.IO.Path.GetExtension(filename) != ".pdf")
-                {
-                    filename += ".pdf";
-                }
+                    int id;
+                    if (!int.TryParse(strInput, out id))
+                    {
+                        Console.WriteLine("Id was not parsable to an int! Try again... \n");
+                        continue;
+                    }
 
-                // Download and save
-                CreatePdf(doujin, filename);
+                    try
+                    {
+                        await GetDoujin(id);
+                    }
+                    catch (InvalidArgumentException)
+                    {
+                        continue;
+                    }
+                }
 
                 Console.WriteLine("Done!\n");
             }
+        }
+
+        private static async Task GetRandom()
+        {
+            string language = "english";
+            var languageTag = SearchClient.GetCategoryTag(language, TagType.Language);
+            var tags = new string[] { languageTag };
+            var result = await SearchClient.SearchWithTagsAsync(tags);
+
+            Console.WriteLine("Number of pages: " + result.numPages);
+            Console.WriteLine("Number per page: " + result.numPerPage);
+
+            var lastPage = await SearchClient.SearchWithTagsAsync(tags, result.numPages);
+            var lastPageLength = lastPage.elements.Length;
+            Console.WriteLine("LastPageLength: " + lastPageLength);
+
+            int totalDoujins = ((result.numPages - 1) * result.numPerPage) + lastPageLength;
+            Console.WriteLine("Total doujins: " + totalDoujins);
+
+            int i = _rnd.Next(1, totalDoujins + 1);
+
+            var temp = await SearchClient.SearchWithTagsAsync(tags, (i / 25) + 1);
+
+            int id = (int)temp.elements[i % 25].id;
+
+            await GetDoujin(id);
+        }
+
+        private static async Task GetRandomWithTags()
+        {
+            Console.Write("Tags: ");
+            var strInput = Console.ReadLine();
+            var tags = (strInput + ",languages:english").Split(",");
+            var result = await SearchClient.SearchWithTagsAsync(tags);
+
+            Console.WriteLine("Number of pages: " + result.numPages);
+            Console.WriteLine("Number per page: " + result.numPerPage);
+
+            var lastPage = await SearchClient.SearchWithTagsAsync(tags);
+            var lastPageLength = lastPage.elements.Length;
+            Console.WriteLine("LastPageLength: " + lastPageLength);
+
+            int totalDoujins = ((result.numPages - 1) * result.numPerPage) + lastPageLength;
+            Console.WriteLine("Total doujins: " + totalDoujins);
+
+            int i = _rnd.Next(1, totalDoujins + 1);
+
+            var temp = await SearchClient.SearchWithTagsAsync(tags);
+
+            int id = (int)temp.elements[i % 25].id;
+
+            await GetDoujin(id);
+        }
+
+        private static async Task GetDoujin(int id)
+        {
+            // Search for doujin and print out info
+            GalleryElement doujin;
+            try
+            {
+                doujin = await SearchClient.SearchByIdAsync(id);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error finding doujin...\n");
+                throw new InvalidArgumentException();
+            }
+
+            PrintDoujinInfo(doujin);
+            Console.WriteLine();
+
+            // Get filename
+            Console.Write("Enter a filename (optional): ");
+            string filename = Console.ReadLine();
+
+            if (filename == string.Empty)
+            {
+                filename = id.ToString();
+            }
+            if (System.IO.Path.GetExtension(filename) != ".pdf")
+            {
+                filename += ".pdf";
+            }
+
+            // Download and save
+            CreatePdf(doujin, filename);
         }
 
         static void PrintDoujinInfo(GalleryElement doujin)
@@ -75,14 +189,21 @@ namespace nh
             Console.WriteLine("Pages:  " + doujin.numPages);
         }
 
-        //static async Task CreateMultiplePdfs(IEnumerable<GalleryElement> doujins)
-        //{
-        //    foreach (var doujin in doujins)
-        //    {
-        //        CreatePdfAsync(doujin, doujin.id.ToString() + ".pdf");
-        //    }
-            
-        //}
+
+        static async Task Multiple(IEnumerable<GalleryElement> doujins)
+        {
+            //var tasks = new List<Task>();
+            //foreach (var doujin in doujins)
+            //{
+            //    tasks.Add(CreatePdfAsync(doujin, doujin.id.ToString() + ".pdf"));
+            //}
+            //Task.WaitAll(tasks.ToArray());
+
+            Parallel.ForEach(doujins, (doujin) => 
+            {
+                CreatePdfSilent(doujin, doujin.id.ToString() + ".pdf");
+            });
+        }
 
         static void CreatePdf(GalleryElement doujin, string filename)
         {
@@ -135,7 +256,7 @@ namespace nh
             Console.WriteLine("Saved successfully!");
         }
 
-        static async Task CreatePdfAsync(GalleryElement doujin, string filename)
+        static void CreatePdfSilent(GalleryElement doujin, string filename)
         {
             // set pdf path/name
             var file = new FileInfo(filename);
@@ -149,8 +270,10 @@ namespace nh
 
 
             // get all images from web
+            Console.WriteLine("Downloading " + doujin.id + "... ");
             var imgUrls = doujin.pages.Select(p => p.imageUrl.ToString()).ToList();
             var imgData = imgUrls.Select(url => ImageDataFactory.Create(url)).ToList();
+            Console.WriteLine("Finished downloading " + doujin.id + "!");
 
             // loop through and add images to document
             Image pdfImg;
